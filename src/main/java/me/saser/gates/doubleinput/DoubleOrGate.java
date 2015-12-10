bd7a0ba4bb95ac4992f509c6eacfcac976b20669
Christian Persson <saser@live.se>
2015-12-10 01:39:38 +0100

    Refactor to extends abstract class instead

--- src/main/java/me/saser/gates/doubleinput/DoubleOrGate.java
+++ src/main/java/me/saser/gates/doubleinput/DoubleOrGate.java
@@ -2,29 +2,10 @@ package me.saser.gates.doubleinput;
 
 import me.saser.gates.Signal;
 
-public class DoubleOrGate implements DoubleInputGate {
-
-    private Signal firstInput, secondInput;
+public class DoubleOrGate extends AbstractDoubleInputGate {
 
     public DoubleOrGate(Signal firstInput, Signal secondInput) {
-        this.firstInput = firstInput;
-        this.secondInput = secondInput;
-    }
-
-    @Override
-    public void setFirstInput(Signal input) {
-        this.firstInput = input;
-    }
-
-    @Override
-    public void setSecondInput(Signal input) {
-        this.secondInput = input;
-    }
-
-    @Override
-    public void setInputs(Signal firstInput, Signal secondInput) {
-        this.setFirstInput(firstInput);
-        this.setSecondInput(secondInput);
+        super(firstInput, secondInput);
     }
 
     @Override
